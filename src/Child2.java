07e50f66e588db136e78213b9add4a7a128a1e28
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-10 16:00:17 +0600

    Update Child2.java

--- src/Child2.java
+++ src/Child2.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Child2 {
+class Child2 extends AbstractParent {
+    private String property2;
+
+    public Child2(String name, int age, String property2) {
+        super(name, age);
+        this.property2 = property2;
+    }
+
+    @Override
+    public void uniqueMethod() {
+        System.out.println(getName() + " has property2: " + property2);
+    }
+
+    private String getName() {
+        return null;
+    }
 }
